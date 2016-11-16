f23c9beb29c6d1aaa4dadc726f92e56358e39db5
Slay3r <florian.durli@yahoo.de>
2016-11-16 11:00:40 +0100

    Präsentation überarbeitet

--- doc/Dokumentation/game.js
+++ doc/Dokumentation/game.js
@@ -9,7 +9,7 @@
  *
  * ---------------------------
  * Menu
- * 
+ * Menu draw in Input & draw prototypes
  * Handle / Manage CSS or HTML variables from JavaScript (Fullscreen,...)
  * ---------------------------
  *
@@ -28,10 +28,14 @@
  * https://www.browserstack.com/
  */
 var i = 0;
-var d = new Date();
+
+var State = { Menu:0, Started:1, Paused:2, Over:3 };
+var GameState = State.Menu;
+var MainMenu;
 
 var debug = true;
 
+// randomizer
 function random(min, max) {
     return Math.round(min + (Math.random() * (max - min)));
 }
@@ -129,6 +133,7 @@ Player.prototype.update = function() {
     // Gravity 
     this.velocityY += 1;
     this.setPosition(this.x + this.velocityX, this.y + this.velocityY);
+
     if (this.y > InfinityRun.height || this.x + this.width < 0) {
         this.x = 150;
         this.y = 50;
@@ -180,7 +185,7 @@ function PlatformManager() {
 
 	
 	//first 3 Platforms execept the Starter Platform
-    /*this.first = new Platform({
+    this.first = new Platform({
         x: 300,
         y: InfinityRun.width / 2,
         width: 400,
@@ -197,26 +202,6 @@ function PlatformManager() {
         y: random(this.second.y - 128, InfinityRun.height - 80),
         width: 400,
         height: 70
-    })*/
-	
-	//flat
-	this.first = new Platform({
-        x: 300,
-        y: 500,
-        width: 1000,
-        height: 70
-    })
-    this.second = new Platform({
-        x: (this.first.x + this.first.width),
-        y: 500,
-        width: 1000,
-        height: 70
-    })
-    this.third = new Platform({
-        x: (this.second.x + this.second.width),
-        y: 500,
-        width: 1000,
-        height: 70
     })
 
     this.first.height = this.first.y + InfinityRun.height;
@@ -231,7 +216,7 @@ function PlatformManager() {
     this.platforms = [this.first, this.second, this.third];
 }
 
-/*PlatformManager.prototype.update = function() {
+PlatformManager.prototype.update = function() {
 
     this.first.x -= 3 + InfinityRun.acceleration;
     if (this.first.x + this.first.width < 0) {
@@ -260,49 +245,18 @@ function PlatformManager() {
         this.third.color = randomChoice(this.colors);
     }
 
-};*/
-
-PlatformManager.prototype.update = function() {
-
-    this.first.x -= 3 + InfinityRun.acceleration;
-    if (this.first.x + this.first.width < 0) {
-        this.first.width = 1000;
-        this.first.x = (this.third.x + this.third.width);
-        //this.first.y = random(this.third.y - 32, InfinityRun.height - 80);
-		this.first.y = 500;
-        this.first.height = this.first.y + InfinityRun.height + 10;
-        this.first.color = randomChoice(this.colors);
-    }
-
-    this.second.x -= 3 + InfinityRun.acceleration;
-    if (this.second.x + this.second.width < 0) {
-        this.second.width = 1000;
-        this.second.x = (this.first.x + this.first.width);
-        //this.second.y = random(this.first.y - 32, InfinityRun.height - 80);
-		this.second.y = 600;
-        this.second.height = this.second.y + InfinityRun.height + 10;
-        this.second.color = randomChoice(this.colors);
-    }
-
-    this.third.x -= 3 + InfinityRun.acceleration;
-    if (this.third.x + this.third.width < 0) {
-        this.third.width = 1000;
-        this.third.x = (this.second.x + this.second.width);
-        //this.third.y = random(this.second.y - 32, InfinityRun.height - 80);
-		this.third.y = 500;
-        this.third.height = this.third.y + InfinityRun.height + 10;
-        this.third.color = randomChoice(this.colors);
-    }
-
 };
+
+
+
 // reset / new Game: set Starting Platform Parameters
 PlatformManager.prototype.updateWhenLose = function() {
 
     this.first.x = 300;
     this.first.color = randomChoice(this.colors);
-    this.first.y = 500;
-    this.second.x = (this.first.x + this.first.width);
-    this.third.x = (this.second.x + this.second.width);
+    this.first.y = InfinityRun.width / random(2, 3);
+    this.second.x = (this.first.x + this.first.width) + random(this.maxDistanceBetween - 150, this.maxDistanceBetween);
+    this.third.x = (this.second.x + this.second.width) + random(this.maxDistanceBetween - 150, this.maxDistanceBetween);
 
 };
 
@@ -330,10 +284,8 @@ Particle.prototype.draw = function() {
 
 /************************************************/
 
-// Initialize default values
 InfinityRun.setup = function() {
 
-	this.distance = 0;
     this.jumpCount = 0;
     this.acceleration = 0;
     this.accelerationTweening = 0;
@@ -356,29 +308,42 @@ InfinityRun.setup = function() {
 
 };
 
+
+
 InfinityRun.update = function() {
 
+	/*switch(GameState){
+		case State.Menu:
+			//InfinityRun.stop();
+			break;
+		case State.Started:
+			break;
+		case State.Paused:
+			break;
+		case State.Over:
+			break;
+	}*/
+	if (GameState == State.Started) {
     this.player.update();
 
     switch (this.jumpCount) {
         case 10:
             this.accelerationTweening = 1;
             this.platformManager.maxDistanceBetween = 430;
-            this.scoreColor = '#076C00';
+            //this.scoreColor = '#076C00';
             break;
         case 25:
             this.accelerationTweening = 2;
             this.platformManager.maxDistanceBetween = 530;
-            this.scoreColor = '#0300A9';
+            //this.scoreColor = '#0300A9';
             break;
         case 40:
             this.accelerationTweening = 3;
             this.platformManager.maxDistanceBetween = 580;
-            this.scoreColor = '#9F8F00';
+            //this.scoreColor = '#9F8F00';
             break;
     }
 
-	//increase acceleration by accelerationTweening
     this.acceleration += (this.accelerationTweening - this.acceleration) * 0.01;
 
 
@@ -432,10 +397,15 @@ InfinityRun.update = function() {
                 if (this.dragging || this.keys.SPACE || this.keys.UP || this.keys.W) {
                     this.player.velocityY = this.player.jumpSize;
                     this.jumpCount++;
+					
                     if (this.jumpCount > this.jumpCountRecord) {
                         this.jumpCountRecord = this.jumpCount;
                     }
                 }
+				/*if (keydown.keys.ESCAPE) {
+					//toggle;
+					InfinityRun.stop;
+				}*/
 
             }
 
@@ -449,12 +419,44 @@ InfinityRun.update = function() {
     for (i = 0; i < this.particles.length; i++) {
         this.particles[i].update();
     };
+}
 
 };
+
+var selectedItem = 0;
+
+InfinityRun.keydown = function() {
+    if (InfinityRun.keys.ESCAPE && GameState==State.Started) {
+		InfinityRun.clear();
+		GameState = State.Menu;
+	} else if (InfinityRun.keys.ESCAPE && GameState==State.Menu) {
+		GameState = State.Started;
+		//InfinityRun.start();
+	}
+	if (InfinityRun.keys.UP) {
+		//var prevSelected = this.selectedItem;
+		selectedItem = (selectedItem + items.length - 1) % items.length;
+	}
+	if (InfinityRun.keys.DOWN) {
+		selectedItem = (selectedItem + 1) % items.length;
+	}
+	
+	if(InfinityRun.keys.ENTER) {
+		callback(selectedItem);
+	}
+	
+}
+
+Menu = function() {
+
+	//this.backgroundCallback = null;
+}
+
 
 //--------- Draw ---------
 
 InfinityRun.draw = function() {
+	if(GameState == State.Started) {
     this.player.draw();
 
     for (i = 0; i < this.platformManager.platforms.length; i++) {
@@ -465,19 +467,66 @@ InfinityRun.draw = function() {
     for (i = 0; i < this.particles.length; i++) {
         this.particles[i].draw();
     };
+	
+	//Draw menu --TODO prototype
+	} else if (GameState == State.Menu) {
+	
+	this.title = "InfinityRun";
+	items = ["Play", "Settings", "Highscore"];
+	
+	callback = function(numItem) { if (numItem == 0) GameState=State.Started };
+	this.height = InfinityRun.height;
+	this.width = InfinityRun.width;
+	this.size = 120;	
+	
+	var lingrad = this.createLinearGradient(0,0,0,this.height);
+	lingrad.addColorStop(0, '#000');
+	lingrad.addColorStop(1, '#023');
+	this.fillStyle = lingrad;
+	this.fillRect(0,0,this.width, this.height)
+	
+	this.textAlign = "center";
+	this.fillStyle = "White";
+	
+	var height = 150;
+	
+	if (this.title) {
+		this.font = Math.floor(this.size*1.3).toString() + "px Times New Roman";
+		this.fillText(this.title, this.width/2, height);
+		height+= height;
+	}
+	
+	for (var i = 0; i < items.length; ++i)
+	{
+		var size = Math.floor(this.size*0.8);
+		if (i == selectedItem)
+		{
+			//var v = Math.floor(127*Math.sin(GameLoopManager.lastTime*0.04) + 127);
+			//this.fillStyle = "rgba(255,255,"+v.toString()+",255)";
+			this.fillStyle = "#A9F5F2";
+			size = this.size+5;
+		}
+		this.font = size.toString() + "px Times New Roman";
+		height += this.size;
+		this.fillText(items[i], InfinityRun.width/2, height);
+		this.fillStyle = "White";
+	}
+	
+	}
+	
 
+	
     //Debug
-
     if (debug) {
         this.font = '12pt Arial';
         this.fillStyle = '#181818';
-        this.fillText('RECORD: ' + this.jumpCountRecord, this.width - 150, 33);
+        this.fillText('Record: ' + this.jumpCountRecord, this.width - 150, 33);
         this.fillStyle = this.scoreColor;
         //this.font = (12 + (this.acceleration * 3))+'pt Arial';
-        this.fillText('JUMPS: ' + this.jumpCount, this.width - 150, 50);
-		
+        this.fillText('Jumps: ' + this.jumpCount, this.width - 150, 50);
 		//todo distance = velocity * time (date: passed time between frames)
-        this.fillText('DISTANCE: ' +0 /* -TODO- */, this.width - 150, 65);
+        this.fillText('Distance: ' + 0/* -TODO- */, this.width - 150, 65);
+		this.fillText('GameState: ' + GameState, this.width - 150, 80);
     }
 
 };
